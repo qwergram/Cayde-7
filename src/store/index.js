4ee08da7609a9fb51a81d6ed64e1ca7bd7831142
Norton Pengra <npengra317@gmail.com>
2018-07-27 09:22:11 -0700

    save 2

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,12 @@ import app from './modules/app'
 
 import * as getters from './getters'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+
 Vue.use(Vuex)
+Vue.use(VueAxios, axios);
 
 const store = new Vuex.Store({
   strict: true, // process.env.NODE_ENV !== 'production',
@@ -14,8 +19,56 @@ const store = new Vuex.Store({
   modules: {
     app
   },
-  state: {},
-  mutations: {}
+  state: {
+    jwt: localStorage.getItem('t'),
+    endpoints: {
+      obtainJWT: process.env.API_ENV + '/api/auth/obtain/',
+      refreshJWT: process.env.API_ENV + '/api/auth/refresh/',
+      verifyJWT: process.env.API_ENV + '/api/auth/verify/'
+    }
+  },
+  mutations: {
+    updateToken(state, newToken) {
+      localStorage.setItem('t', newToken)
+      state.jwt = newToken
+    },
+    removeToken(state) {
+      localStorage.removeItem('t')
+      state.jwt = null
+    }
+  },
+  actions: {
+    obtainToken(username, password) {
+      const payload = {
+        username: username,
+        password: password
+      }
+      axios.post(this.state.endpoints.obtainJWT, payload)
+        .then((response) => {
+          this.commit('updateToken', response.data.token)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    refreshToken() {
+      const payload = {
+        token: this.state.jwt
+      }
+
+      axios.post(this.state.endpoints.refreshJWT, payload)
+        .then((response) => {
+          this.commit('updateToken', response.data.token)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    inspectToken() {
+      const token = this.state.jwt
+      // https://hackernoon.com/jwt-authentication-in-vue-js-and-django-rest-framework-part-2-788f0ad53ad5
+    }
+  }
 })
 
 Vue.use(VuexI18n.plugin, store)
