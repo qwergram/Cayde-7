488c0c3fcbf794213494159d961a1984bb4bf501
Norton Pengra <npengra317@gmail.com>
2018-07-29 01:03:05 -0700

    remove default views

--- src/router/index.js
+++ src/router/index.js
@@ -7,9 +7,9 @@ import store from '../store/index'
 
 Vue.use(Router)
 
-const EmptyParentComponent = {
-  template: '<router-view></router-view>'
-}
+// const EmptyParentComponent = {
+//   template: '<router-view></router-view>'
+// }
 
 const router = new Router({
   routes: [
@@ -24,12 +24,14 @@ const router = new Router({
         {
           name: 'login',
           path: 'login',
-          component: lazyLoading('auth/login/Login')
+          component: lazyLoading('auth/login/Login'),
+          meta: { authPage: true }
         },
         {
           name: 'signup',
           path: 'signup',
-          component: lazyLoading('auth/signup/Signup')
+          component: lazyLoading('auth/signup/Signup'),
+          meta: { authPage: true }
         },
         {
           path: '',
@@ -39,198 +41,210 @@ const router = new Router({
     },
     {
       name: 'Admin',
-      path: '/admin',
+      path: '/dash',
       component: AppLayout,
       meta: { auth: true },
       children: [
         {
           name: 'dashboard',
-          path: 'dashboard',
+          path: '',
           component: lazyLoading('dashboard/Dashboard'),
           default: true,
-          meta: { auth: true },
-        },
-        {
-          path: 'statistics',
-          component: EmptyParentComponent,
-          children: [
-            {
-              name: 'charts',
-              path: 'charts',
-              component: lazyLoading('statistics/charts/Charts')
-            },
-            {
-              name: 'progress-bars',
-              path: 'progress-bars',
-              component: lazyLoading('statistics/progress-bars/ProgressBars'),
-            },
-            {
-              path: '',
-              redirect: {name: 'charts'}
-            }
-          ]
-        },
-        {
-          path: 'forms',
-          component: EmptyParentComponent,
-          children: [
-            {
-              name: 'form-elements',
-              path: 'form-elements',
-              component: lazyLoading('forms/form-elements/FormElements')
-            },
-            {
-              name: 'form-wizards',
-              path: 'form-wizards',
-              component: lazyLoading('forms/form-wizard/FormWizard')
-            },
-            {
-              name: 'medium-editor',
-              path: 'medium-editor',
-              component: lazyLoading('forms/medium-editor/MediumEditor')
-            },
-            {
-              path: '',
-              redirect: {name: 'form-elements'}
-            }
-          ]
-        },
-        {
-          name: 'tables',
-          path: 'tables',
-          component: lazyLoading('tables/Table')
-        },
-        {
-          path: 'ui',
-          component: EmptyParentComponent,
-          children: [
-            {
-              name: 'typography',
-              path: 'typography',
-              component: lazyLoading('ui/typography/Typography')
-            },
-            {
-              name: 'buttons',
-              path: 'buttons',
-              component: lazyLoading('ui/buttons/Buttons')
-            },
-            {
-              name: 'color-pickers',
-              path: 'color-pickers',
-              component: lazyLoading('ui/color-pickers/ColorPickers')
-            },
-            {
-              name: 'notifications',
-              path: 'notifications',
-              component: lazyLoading('ui/notifications/Notifications')
-            },
-            {
-              path: 'icons',
-              component: lazyLoading('ui/icons/Icons'),
-              children: [
-                {
-                  name: 'icons',
-                  path: '', // Default route
-                  component: lazyLoading('ui/icons/SetsList')
-                },
-                {
-                  path: ':name',
-                  component: lazyLoading('ui/icons/Set'),
-                  props: true
-                }
-              ]
-            },
-            {
-              name: 'spinners',
-              path: 'spinners',
-              component: lazyLoading('ui/spinners/Spinners')
-            },
-            {
-              name: 'grid',
-              path: 'grid',
-              component: lazyLoading('ui/grid/Grid')
-            },
-            {
-              name: 'modals',
-              path: 'modals',
-              component: lazyLoading('ui/modals/Modals')
-            },
-            {
-              name: 'cards',
-              path: 'cards',
-              component: lazyLoading('ui/cards/Cards')
-            },
-            {
-              name: 'file-upload',
-              path: 'file-upload',
-              component: lazyLoading('ui/file-upload/FileUpload')
-            },
-            {
-              name: 'tags',
-              path: 'tags',
-              component: lazyLoading('ui/tags/Tags')
-            },
-            {
-              name: 'tree-view',
-              path: 'tree-view',
-              component: lazyLoading('ui/tree-view/TreeView')
-            },
-            {
-              path: '',
-              redirect: {name: 'typography'}
-            }
-          ]
-        },
-        {
-          name: 'extra',
-          path: 'extra',
-          component: lazyLoading('extra/Extra')
-        },
-        {
-          path: 'maps',
-          component: EmptyParentComponent,
-          children: [
-            {
-              name: 'google-maps',
-              path: 'google-maps',
-              component: lazyLoading('maps/google-maps/GoogleMapsPage')
-            },
-            {
-              name: 'yandex-maps',
-              path: 'yandex-maps',
-              component: lazyLoading('maps/yandex-maps/YandexMapsPage')
-            },
-            {
-              name: 'leaflet-maps',
-              path: 'leaflet-maps',
-              component: lazyLoading('maps/leaflet-maps/LeafletMapsPage'),
-            },
-            {
-              name: 'bubble-maps',
-              path: 'bubble-maps',
-              component: lazyLoading('maps/bubble-maps/BubbleMapsPage'),
-            },
-            {
-              name: 'line-maps',
-              path: 'line-maps',
-              component: lazyLoading('maps/line-maps/LineMapsPage')
-            },
-            {
-              path: '',
-              redirect: {name: 'google-maps'}
-            }
-          ]
+          meta: { auth: true }
         }
       ]
+      // children: [
+      //   {
+      //     name: 'dashboard',
+      //     path: 'dashboard',
+      //     component: lazyLoading('dashboard/Dashboard'),
+      //     default: true,
+      //     meta: { auth: true },
+      //   },
+      //   {
+      //     path: 'statistics',
+      //     component: EmptyParentComponent,
+      //     children: [
+      //       {
+      //         name: 'charts',
+      //         path: 'charts',
+      //         component: lazyLoading('statistics/charts/Charts')
+      //       },
+      //       {
+      //         name: 'progress-bars',
+      //         path: 'progress-bars',
+      //         component: lazyLoading('statistics/progress-bars/ProgressBars'),
+      //       },
+      //       {
+      //         path: '',
+      //         redirect: {name: 'charts'}
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     path: 'forms',
+      //     component: EmptyParentComponent,
+      //     children: [
+      //       {
+      //         name: 'form-elements',
+      //         path: 'form-elements',
+      //         component: lazyLoading('forms/form-elements/FormElements')
+      //       },
+      //       {
+      //         name: 'form-wizards',
+      //         path: 'form-wizards',
+      //         component: lazyLoading('forms/form-wizard/FormWizard')
+      //       },
+      //       {
+      //         name: 'medium-editor',
+      //         path: 'medium-editor',
+      //         component: lazyLoading('forms/medium-editor/MediumEditor')
+      //       },
+      //       {
+      //         path: '',
+      //         redirect: {name: 'form-elements'}
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     name: 'tables',
+      //     path: 'tables',
+      //     component: lazyLoading('tables/Table')
+      //   },
+      //   {
+      //     path: 'ui',
+      //     component: EmptyParentComponent,
+      //     children: [
+      //       {
+      //         name: 'typography',
+      //         path: 'typography',
+      //         component: lazyLoading('ui/typography/Typography')
+      //       },
+      //       {
+      //         name: 'buttons',
+      //         path: 'buttons',
+      //         component: lazyLoading('ui/buttons/Buttons')
+      //       },
+      //       {
+      //         name: 'color-pickers',
+      //         path: 'color-pickers',
+      //         component: lazyLoading('ui/color-pickers/ColorPickers')
+      //       },
+      //       {
+      //         name: 'notifications',
+      //         path: 'notifications',
+      //         component: lazyLoading('ui/notifications/Notifications')
+      //       },
+      //       {
+      //         path: 'icons',
+      //         component: lazyLoading('ui/icons/Icons'),
+      //         children: [
+      //           {
+      //             name: 'icons',
+      //             path: '', // Default route
+      //             component: lazyLoading('ui/icons/SetsList')
+      //           },
+      //           {
+      //             path: ':name',
+      //             component: lazyLoading('ui/icons/Set'),
+      //             props: true
+      //           }
+      //         ]
+      //       },
+      //       {
+      //         name: 'spinners',
+      //         path: 'spinners',
+      //         component: lazyLoading('ui/spinners/Spinners')
+      //       },
+      //       {
+      //         name: 'grid',
+      //         path: 'grid',
+      //         component: lazyLoading('ui/grid/Grid')
+      //       },
+      //       {
+      //         name: 'modals',
+      //         path: 'modals',
+      //         component: lazyLoading('ui/modals/Modals')
+      //       },
+      //       {
+      //         name: 'cards',
+      //         path: 'cards',
+      //         component: lazyLoading('ui/cards/Cards')
+      //       },
+      //       {
+      //         name: 'file-upload',
+      //         path: 'file-upload',
+      //         component: lazyLoading('ui/file-upload/FileUpload')
+      //       },
+      //       {
+      //         name: 'tags',
+      //         path: 'tags',
+      //         component: lazyLoading('ui/tags/Tags')
+      //       },
+      //       {
+      //         name: 'tree-view',
+      //         path: 'tree-view',
+      //         component: lazyLoading('ui/tree-view/TreeView')
+      //       },
+      //       {
+      //         path: '',
+      //         redirect: {name: 'typography'}
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     name: 'extra',
+      //     path: 'extra',
+      //     component: lazyLoading('extra/Extra')
+      //   },
+      //   {
+      //     path: 'maps',
+      //     component: EmptyParentComponent,
+      //     children: [
+      //       {
+      //         name: 'google-maps',
+      //         path: 'google-maps',
+      //         component: lazyLoading('maps/google-maps/GoogleMapsPage')
+      //       },
+      //       {
+      //         name: 'yandex-maps',
+      //         path: 'yandex-maps',
+      //         component: lazyLoading('maps/yandex-maps/YandexMapsPage')
+      //       },
+      //       {
+      //         name: 'leaflet-maps',
+      //         path: 'leaflet-maps',
+      //         component: lazyLoading('maps/leaflet-maps/LeafletMapsPage'),
+      //       },
+      //       {
+      //         name: 'bubble-maps',
+      //         path: 'bubble-maps',
+      //         component: lazyLoading('maps/bubble-maps/BubbleMapsPage'),
+      //       },
+      //       {
+      //         name: 'line-maps',
+      //         path: 'line-maps',
+      //         component: lazyLoading('maps/line-maps/LineMapsPage')
+      //       },
+      //       {
+      //         path: '',
+      //         redirect: {name: 'google-maps'}
+      //       }
+      //     ]
+      //   }
+      // ]
     }
   ]
 })
 
 router.beforeEach((to, from, next) => {
   const authRequired = to.matched.some((route) => route.meta.auth)
+  const authPage = to.matched.some((route) => route.meta.authPage)
   const authed = store.state.user
   if (authRequired && !authed) {
     next('/auth/login')
+  } else if (authPage && authed) {
+    next('/dash')
   } else {
     next()
   }
